f4ee6470f026afc016118f847db238fe672a94ce
Salvador Hernandez Lopez <chavatloz@gmail.com>
2021-04-26 18:49:34 -0500

    Add a list of categories

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const GifExpertApp = () => {
+  const initialState = ['Vikings', 'Peacky Bliders', 'The last air bender'];
+  const [categories, setCategory] = useState(initialState);
+  const handleAdd = () => {
+    setCategory((cats) => [...cats, 'new Serie']);
+  };
   return (
     <div>
       <h2>Gif Expert App </h2>
       <hr />
+      <button onClick={handleAdd}> Agregar </button>
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </div>
   );
 };
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const AddCategory = () => {
+  return (
+    <div>
+      <h2>Gif Expert App </h2>
+    </div>
+  );
+};
+
+export default AddCategory;
