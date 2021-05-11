ff8501c09e08274c014f6447451ab388794900c5
Salvador Hernandez Lopez <chavatloz@gmail.com>
2021-05-11 16:31:15 -0500

    test useFetch

--- src/test/hooks/useFetchGifs.test.js
+++ src/test/hooks/useFetchGifs.test.js
@@ -3,11 +3,19 @@ import useFetchGifs from '../../hooks/useFetchGifs';
 import { renderHook } from '@testing-library/react-hooks';
 
 describe('tests in useFetrchGifs', () => {
-  it('should return the initial state', () => {
-    const { result } = renderHook( () => useFetchGifs('test') )  
+  it('should return the initial state', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('test'))
     const { images, loading } = result.current;
+    await waitForNextUpdate();
     expect(images).toEqual([]);
     expect(loading).toBeTruthy();
+  });
+  it('should return a list of images and the loading false', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('test'))
+    await waitForNextUpdate();
+    const { images, loading } = result.current;
+    expect(images.length).toBe(10);
+    expect(loading).toBe(false);
   });
 });
 
